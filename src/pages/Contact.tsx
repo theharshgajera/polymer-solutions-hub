a5d72880dc7a22e5aa235c6556102fbc3a850f68
theharshgajera <harshgajera.ict21@aii.ac.in>
2026-05-04 18:23:14 +0530

    c

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -24,10 +24,10 @@ const ContactPage = () => {
         <div className="container mx-auto px-4">
           <div className="rounded-2xl overflow-hidden shadow-lg border border-border">
             <iframe
-              title="Multi-Tech Polymers Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235013.70717963824!2d72.41395065!3d23.020474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e848aba5bd449%3A0x4fcedd11614f6516!2sAhmedabad%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin"
+              title="Multi-Tech Location"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1920.55566499301!2d72.62459684399293!3d23.013806241840566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e86872fffffff%3A0xd7576ed854a038a!2sMulti-tech!5e0!3m2!1sen!2sin!4v1777899154985!5m2!1sen!2sin"
               width="100%"
-              height="400"
+              height="450"
               style={{ border: 0 }}
               allowFullScreen
               loading="lazy"
